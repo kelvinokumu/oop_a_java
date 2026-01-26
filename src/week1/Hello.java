91bb3ed6e1097ef2f8dd35ba196a589b1e220522
kelvinokumu <oumakelvinokumu@gmail.com>
2026-01-26 08:27:13 +0300

    data types

---
+++ src/week1/Hello.java
@@ -0,0 +1,46 @@
+package week1;
+
+
+
+public class Hello 
+{
+    public static void main(String[] args){
+
+        int c, d, e; //multiple declaration of same datatype
+
+        int a;          // declaration
+        a = 25;         //assignment
+
+        
+        double price;
+        float price1;
+        // bool flag;
+
+        int age = 25;    // declaration and assignment
+        String name = "Sean";
+
+        System.out.println("The name is "+name+" age is "+age);
+
+
+        sum();
+        Hello h1 = new Hello();
+        h1.getResults();
+
+
+        // Scanner sc = new Scanner(System.in);
+        
+    }
+
+
+    public static int sum(){
+
+        return 0;
+    }
+
+
+    int getResults(){
+
+        return 0;
+    }
+    
+}
